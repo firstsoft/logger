dcb6108d91e3293a95784ddb2d68f40341f25d3f
AMECN\yashanyang <yashanyang@amecnsh.com>
2017-04-26 11:48:47 +0800

    no message

---
+++ source/DataTransferService/DTS/DTLib/log/collection/Base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTLib.log.collection
+{
+    public class Base
+    {
+        public bool deleted { get; set; }
+        public DateTime createtime { get; set; }
+        public string creator { get; set; }
+        public DateTime modifietime { get; set; }
+        public string revisor { get; set; }
+
+        public Base()
+        {
+            this.deleted = false;
+            this.createtime = DateTime.Now;
+            this.modifietime = DateTime.Now;
+        }
+    }
+}
---
+++ source/DataTransferService/DTS/DTServiceHost/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace DTServiceHost
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/");
+            this.astroHost = new WebServiceHost(typeof(DTService.Service), baseAddress);
+            this.astroHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            this.astroHost.Close();
+        }
+
+        private WebServiceHost astroHost;
+    }
+}
