61eac86989949ed90a4a3d6010fdb15d9ee3499c
AMECN\yashanyang <yashanyang@amecnsh.com>
2017-04-28 11:16:06 +0800

    no message

--- source/DataTransferService/DTS/DTService/Service.cs
+++ source/DataTransferService/DTS/DTService/Service.cs
@@ -26,25 +26,29 @@ namespace DTService
 
         public void SetLogUri(string value)
         {
+            byte[] outputb = Convert.FromBase64String(value);
+            string orgStr = Encoding.Default.GetString(outputb);
             //@"E:\Work\yashanyang_view10\Casino\AH\Data\Log"
             DTEntry.looper.SendMsg(new DTLib.core.Message("set cfg path", () =>
             {
                 if (!DTEntry.Setters.ContainsKey(typeof(CfgLogPathSetter)))
                 {
                     CfgLogPathSetter cfgPathRegister = new CfgLogPathSetter();
-                    cfgPathRegister.Set(value);
+                    cfgPathRegister.Set(orgStr);
                 }
             }));
         }
         public void SetDBUri(string value)
         {
             //mongodb://192.168.15.113:27017@Events
+            byte[] outputb = Convert.FromBase64String(value);
+            string orgStr = Encoding.Default.GetString(outputb);
             DTEntry.looper.SendMsg(new DTLib.core.Message("set connection", () =>
             {
                 if (!DTEntry.Setters.ContainsKey(typeof(CfgDbConnectionSetter)))
                 {
                     CfgDbConnectionSetter cfgProvider = new CfgDbConnectionSetter();
-                    cfgProvider.Set(value);
+                    cfgProvider.Set(orgStr);
                 }
             }));
         }
